709893bd0e8b46839c2ef9495721d5c7a929411c
Honye <hongye.jun@qq.com>
2024-06-20 15:32:19 +0800

    vmess cipher

--- src/app/sub/route.js
+++ src/app/sub/route.js
@@ -5,6 +5,23 @@ const subscription = process.env.SUBSCRIPTION_URL
 const confPath = path.resolve(process.cwd(), 'src/templates', 'config.ejs')
 
 /**
+ * 是否存在值
+ * @template T
+ * @param {T} value
+ * @returns {value is NonNullable<T>}
+ */
+const isPresent = (value) => typeof value !== 'undefined' && value !== null
+
+/**
+ * @template T
+ * @param {T} value
+ * @param {NonNullable<T>} defaultValue
+ */
+const getIfPresent = (value, defaultValue) => {
+  return isPresent(value) ? value : defaultValue
+}
+
+/**
  * @param {string} url
  */
 const fetchProxies = async (url) => {
@@ -20,6 +37,19 @@ const fetchProxies = async (url) => {
       const [_, cipher, password, server, port] = decry.match(regex)
       return { name, server, port, cipher, password, type: 'ss' }
     } else if (/^vmess:\/\//.test(line)) {
+      /**
+       * @typedef {object} Vmess
+       * @property {string} ps
+       * @property {string} port
+       * @property {string} id
+       * @property {number} aid
+       * @property {string} net
+       * @property {string} type
+       * @property {'none'|boolean} tls
+       * @property {string} add
+       * @property {string} [scy]
+       */
+      /** @type {Vmess} */
       const decry = JSON.parse(atob(line.substring(8)))
       let name = decry.ps
       name = name.match(/@(.*?)\./)[1]
@@ -30,8 +60,9 @@ const fetchProxies = async (url) => {
         type: 'vmess',
         uuid: decry.id,
         alterId: decry.aid,
-        udp: decry.net.includes('udp'),
-        tls: decry.tls === true
+        udp: true,
+        tls: decry.tls === true,
+        cipher: getIfPresent(decry.scy, 'auto')
       }
     }
   })
